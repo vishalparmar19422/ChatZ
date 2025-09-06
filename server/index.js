29f601f992bfb37f7e7ef081849144c0ebc97299
vishalparmar19422 <parmavishal1942@gmail.com>
2025-09-07 01:19:49 +0530

    fix: Remove .env files and correct .gitignore location

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "http://localhost:5173" } });
+const io = new Server(server, { cors: { origin: ["http://localhost:5173" , "https://chatz-1.onrender.com/"] } });
 
 const rooms = new Map();
 
