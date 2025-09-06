5ff539affdd209ccb826132243bd2f60459c9c66
vishalparmar19422 <parmavishal1942@gmail.com>
2025-09-07 01:36:02 +0530

    bug solved

--- server/index.js
+++ server/index.js
@@ -9,12 +9,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: ["http://localhost:5173", "https://chatz-1.onrender.com/"] },
+  cors: { origin: ["http://localhost:5173", "https://chatz-1.onrender.com"] },
 });
 
 const rooms = new Map();
 
-app.use(cors({ origin:["http://localhost:5173", "https://chatz-1.onrender.com/"] }));
+app.use(cors({ origin:["http://localhost:5173", "https://chatz-1.onrender.com"] }));
 app.use(express.json());
 
 io.on("connection", (socket) => {
